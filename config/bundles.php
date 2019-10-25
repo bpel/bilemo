c9007d6c2dc95db2720c89a2fb044919248d616d
benjamin.pelletier <pelletierbenjamin90@gmail.com>
2019-10-25 11:07:12 +0200

    Update packages #20 + delete colourRepository

--- config/bundles.php
+++ config/bundles.php
@@ -6,4 +6,5 @@ return [
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
+    Symfony\Bundle\WebServerBundle\WebServerBundle::class => ['dev' => true],
 ];
